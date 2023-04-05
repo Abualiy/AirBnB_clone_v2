bf6042fd27c3919f3549f4009b5c9957c1dca3b8
Abualiy <akremmuktar332@gmail.com>
2023-04-05 11:03:29 +0000

    update

---
+++ 1-pack_web_static.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from fabric.api import local
+from time import strftime
+from datetime import date
+
+
+def do_pack():
+    """Fabric script that generates archive from the contents of web_static"""
+
+
+    filename = strf
