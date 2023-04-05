31e08e08b6c9cae4931adaec45d7d28c92c4ac6d
Abualiy <akremmuktar332@gmail.com>
2023-04-05 16:35:31 +0000

    AirBnB_clone_v2

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-from fabric.api import local
-from time import strftime
-from datetime import date
+from fabric.api import *
+import os
+from datetime import datetime
+
+env.hosts = ['localhost']
 
 
 def do_pack():
-    """Fabric script that generates archive from the contents of web_static"""
-
-    filename = strftime("%Y%m%d%H%M%S")
     try:
+        filepath = "versions/web_static_" + datetime.now().\
+                   strftime("%Y%m%d%H%M%S") + ".tgz"
         local("mkdir -p versions")
-        local("tar -czvf versions/web_static_{}.tgz web_static/"
-                .format(filename))
-
-        return "versions/web_static_{}.tgz".format(filename)
-
-    except Exception as e:
-        return None
+        local("tar -zcvf versions/web_static_$(date +%Y%m%d%H%M%S).tgz\
+        web_static")
+        print("web_static packed: {} -> {}".
+              format(filepath, os.path.getsize(filepath)))
+    except:
+            return None
